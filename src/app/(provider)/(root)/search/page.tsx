c72815ef8063fb9e1c492dd2a9e634aa1150b6af
muyeon <gmakin36@gmail.com>
2024-07-22 16:53:03 +0900

    검색 페이지 초기 구성 - 기능 없이 간단하게 우선 구성 - TODO : api 호출 추가 - TODO : 쿼리 스트링을 활용한 검색 기능 추가 - TODO : 검색 결과로 받아온 데이터 화면에 그려주는 로직 추가

--- src/app/(provider)/(root)/search/page.tsx
+++ src/app/(provider)/(root)/search/page.tsx
@@ -1,5 +1,23 @@
-const page = () => {
-  return <div>page</div>;
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
+const SearchPage = () => {
+  return (
+    <div>
+      <section className="flex justify-between items-center mb-4">
+        <Button>{'<'}</Button>
+        <h2 className="flew-grow text-center">검색</h2>
+        <div className="w-10"></div>
+      </section>
+      <section className="flex justify-center items-center">
+        <Input />
+        <Button>검색</Button>
+      </section>
+      <section>최근 검색어</section>
+      <section>인기 검색어</section>
+      <section>검색 결과 리스트</section>
+    </div>
+  );
 };
 
-export default page;
+export default SearchPage;
