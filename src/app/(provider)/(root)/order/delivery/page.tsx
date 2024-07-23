9e006a5ffedc9640bdf1b6efaedc357cc391e498
박원빈 <harry21@kakao.com>
2024-07-23 19:40:19 +0900

    Del: Order Context 삭제

--- src/app/(provider)/(root)/order/delivery/page.tsx
+++ src/app/(provider)/(root)/order/delivery/page.tsx
@@ -1,14 +1,11 @@
 import { OrderButton, OrderForm, ReceiverForm } from '@/components/OrderPage';
-import OrderProvider from './_context/OrderContext';
 
 const DeliveryPage = () => {
   return (
     <div className="max-w-[600px] mx-auto flex flex-col gap-4">
-      <OrderProvider>
-        <OrderForm />
-        <ReceiverForm />
-        <OrderButton />
-      </OrderProvider>
+      <OrderForm />
+      <ReceiverForm />
+      <OrderButton />
     </div>
   );
 };
