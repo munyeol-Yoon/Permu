6ba3850b1cf945ef8e41483546117666cdc73c00
박원빈 <harry21@kakao.com>
2024-07-24 15:32:03 +0900

    Add: useOrderInfoQuery 커스텀 훅 추가, 구조 변경 - 기존 컴포넌트화가 되어있던 구조에서 컴포넌트화가 되어있지 않은 구조로 변경하였습니다. - useRef를 활용하여 Input의 상태를 가져옵니다.

--- src/app/(provider)/(root)/order/delivery/page.tsx
+++ src/app/(provider)/(root)/order/delivery/page.tsx
@@ -1,17 +1,74 @@
-import { getOrderInfoByUserId } from '@/api/orderInfo';
-import { OrderButton, OrdererForm, ReceiverForm } from '@/components/OrderPage';
-import ProductList from '@/components/OrderPage/ProductList';
+'use client';
 
-const DeliveryPage = async () => {
-  // TODO: 하드코딩되어있는 userId 수정
-  const { productList, user } = await getOrderInfoByUserId('d075a7dd-98bb-4e69-9209-e03c6057a901');
+import ProductItem from '@/components/OrderPage/ProductItem';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import useOrderInfoQuery from '@/hooks/query/useOrderInfoQuery';
+import { useRef } from 'react';
+
+// 솔루션 1 : useRef 활용 -> 리렌더링 이슈 최소화
+// 솔루션 2 : 컴포넌화 굳이 할 필요 없으니까 하지않는다
+
+const DeliveryPage = () => {
+  const { data: orderInfo } = useOrderInfoQuery();
+
+  const receiverNameRef = useRef('');
+  const receiverAddressRef = useRef('');
+  const receiverPhoneNumberRef = useRef('');
+  const receiverMemoRef = useRef('');
+
+  const handleClick = () => {
+    console.log(receiverNameRef);
+  };
 
   return (
     <div className="max-w-[600px] mx-auto flex flex-col gap-4">
-      <ProductList productList={productList} />
-      <OrdererForm user={user} />
-      <ReceiverForm />
-      <OrderButton />
+      <ul>{orderInfo?.productList.map((product) => <ProductItem key={product.productId} />)}</ul>
+
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl">주문 정보</h1>
+
+        <div>
+          <label htmlFor="orderer_name">주문자</label>
+          <p id="orderer_name">{orderInfo?.user.name}</p>
+        </div>
+
+        <div>
+          <label htmlFor="orderer_phone_number">휴대폰번호</label>
+          <p id="orderer_name">{orderInfo?.user.phone}</p>
+        </div>
+
+        <div>
+          <label htmlFor="orderer_email">이메일</label>
+          <p id="orderer_name">{orderInfo?.user.email}</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl">배송 정보</h1>
+
+        <div>
+          <label htmlFor="receiver_name">배송받을 분</label>
+          <Input id="receiver_name" onChange={(e) => (receiverNameRef.current = e.target.value)} />
+        </div>
+
+        <div>
+          <label htmlFor="receiver_address">배송받을 주소</label>
+          <Input id="receiver_address" onChange={(e) => (receiverAddressRef.current = e.target.value)} />
+        </div>
+
+        <div>
+          <label htmlFor="receiver_phone_number">휴대폰번호</label>
+          <Input id="receiver_phone_number" onChange={(e) => (receiverPhoneNumberRef.current = e.target.value)} />
+        </div>
+
+        <div>
+          <label htmlFor="receiver_memo">배송 메모</label>
+          <Input id="receiver_memo" onChange={(e) => (receiverMemoRef.current = e.target.value)} />
+        </div>
+      </div>
+
+      <Button onClick={handleClick}>주문하기</Button>
     </div>
   );
 };
---
+++ src/hooks/query/useOrderInfoQuery.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+
+const useOrderInfoQuery = () => {
+  // TODO: 하드코딩되어있는 userId 수정
+  const result = useQuery({
+    queryKey: ['orderInfo'],
+    queryFn: async () => {
+      const res = await fetch('/api/orderList/d075a7dd-98bb-4e69-9209-e03c6057a901');
+      const data = await res.json();
+      return data;
+    }
+  });
+
+  return result;
+};
+
+export default useOrderInfoQuery;
