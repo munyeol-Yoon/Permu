4527a27a8278932c5b4af84df72f6987adbfa185
박원빈 <harry21@kakao.com>
2024-07-25 16:53:15 +0900

    Refactor: hooks 내 파일 구조 변경

--- src/hooks/query/index.ts
+++ src/hooks/query/index.ts
@@ -1,3 +1,5 @@
+export { default as useCartsQuery } from './useCartsQuery';
 export { default as useCouponQuery } from './useCouponQuery';
 export { default as useOrderInfoQuery } from './useOrderInfoQuery';
 export { default as useSearchQuery } from './useSearchQuery';
+export { default as useWishesQuery } from './useWishesQuery';
