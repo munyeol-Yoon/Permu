97ccc7507da8da2de0997de74aeb585451b73559
박원빈 <harry21@kakao.com>
2024-07-25 11:20:19 +0900

    Add: 주문 완료페이지 ui 작업 - 간단한 주문완료 ui와 링크를 추가하였습니다.

--- src/app/(provider)/(root)/order/complete/page.tsx
+++ src/app/(provider)/(root)/order/complete/page.tsx
@@ -1,5 +1,14 @@
+import Link from 'next/link';
+
 const CompletePage = () => {
-  return <div>CompletePage</div>;
+  return (
+    <div className="max-w-[600px] mx-auto flex flex-col gap-4 py-12">
+      <h1 className="text-2xl font-bold">주문이 완료되었습니다.</h1>
+      <Link href="/user/orders" className="text-blue-400 hover:brightness-75">
+        내 주문내역 보러가기
+      </Link>
+    </div>
+  );
 };
 
 export default CompletePage;
