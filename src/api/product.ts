d1f6380802ba62fffa1cd3301d77822674011be5
leeejin <asd72621@naver.com>
2024-07-25 15:54:41 +0900

    Fix: products/product로 수정

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,7 +1,9 @@
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
 
 export const fetchDetailProduct = async ({ params }: Params): Promise<Product> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products?productId=${params.productId}`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/products/product?productId=${params.productId}`
+  );
   const data = await response.json();
   return data;
 };
