7cc8003ee0340a8d9c0986b676b31bbc372aa821
leeejin <asd72621@naver.com>
2024-07-26 19:37:23 +0900

    Update : Accordion 컴포넌트 수정 - 공통 스타일 global에 추가

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -45,11 +45,10 @@ const AccordionContent = React.forwardRef<
     className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
-    <div className={cn('pb-4 pt-0', className)}>{children}</div>
+    <div className={cn('px-5 py-4', className)}>{children}</div>
   </AccordionPrimitive.Content>
 ));
 
 AccordionContent.displayName = AccordionPrimitive.Content.displayName;
 
 export { Accordion, AccordionContent, AccordionItem, AccordionTrigger };
-
