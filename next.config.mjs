6cfdb1c84e07ff2c8a4505fddf57b7c2380dd6aa
muyeon <gmakin36@gmail.com>
2024-07-29 02:49:35 +0900

    Chore: SVG 관련 세팅 및 타입 설정 - SVG 타입 추론 - import 별칭 추가

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     remotePatterns: [{ hostname: 'i.pinimg.com' }]
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
   }
 };
 
--- src/app/(provider)/(root)/search/layout.tsx
+++ src/app/(provider)/(root)/search/layout.tsx
@@ -1,9 +1,13 @@
+import LogoSVG from '@@/public/logo.svg';
 import { PropsWithChildren } from 'react';
+// TODO: 경로 논의 필요 @를 사용해야하지만 더 상위에 있는 폴더인점
 
 const SearchPageLayout = ({ children }: PropsWithChildren) => {
   return (
     <div>
-      <header>permeate</header>
+      <header>
+        <LogoSVG />
+      </header>
       {children}
     </div>
   );
