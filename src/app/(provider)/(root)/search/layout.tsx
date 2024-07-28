63920f7f6c209e73f65a53f2fc997c5b0bac0cf2
muyeon <gmakin36@gmail.com>
2024-07-29 03:26:22 +0900

    Design: SearchPage 헤더 레이아웃 UI

--- src/app/(provider)/(root)/search/layout.tsx
+++ src/app/(provider)/(root)/search/layout.tsx
@@ -1,13 +1,10 @@
-import LogoSVG from '@@/public/logo.svg';
+import SearchHeader from '@/components/SearchPage/SearchHeader';
 import { PropsWithChildren } from 'react';
-// TODO: 경로 논의 필요 @를 사용해야하지만 더 상위에 있는 폴더인점
 
 const SearchPageLayout = ({ children }: PropsWithChildren) => {
   return (
     <div>
-      <header>
-        <LogoSVG />
-      </header>
+      <SearchHeader />
       {children}
     </div>
   );
