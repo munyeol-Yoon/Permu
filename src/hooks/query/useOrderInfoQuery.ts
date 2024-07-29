ce368c728c8c8d83fe3336f189195c522be6704b
박원빈 <harry21@kakao.com>
2024-07-29 14:46:16 +0900

    Fix: BASEURL 주소 추가

--- src/hooks/query/useOrderInfoQuery.ts
+++ src/hooks/query/useOrderInfoQuery.ts
@@ -7,7 +7,7 @@ const useOrderInfoQuery = () => {
   return useQuery({
     queryKey: ['orderInfo', loggedUser?.id],
     queryFn: async () => {
-      const res = await fetch(`/api/orderInfo/${loggedUser?.id ?? ''}`);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/orderInfo/${loggedUser?.id ?? ''}`);
       const data = await res.json();
       return data;
     }
