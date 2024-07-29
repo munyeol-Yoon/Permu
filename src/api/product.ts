6bba00fbec7eb3fdd0bf9527f263bdaba6f9b53b
leeejin <asd72621@naver.com>
2024-07-29 16:42:13 +0900

    Fix : merge반영 안된 코드 수정

--- src/api/product.ts
+++ src/api/product.ts
@@ -23,7 +23,7 @@ export const getRelatedSearchProducts = async (search: string) => {
   return data;
 };
 
-export const fetchDetailProduct = async (productId: string): Promise<Product> => {
+export const getDetailProduct = async (productId: string): Promise<Product> => {
   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products/product?productId=${productId}`);
   const data = await response.json();
   return data;
