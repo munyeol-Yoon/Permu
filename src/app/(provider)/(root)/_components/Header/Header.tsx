6d4cc3d814058be4de034359a7644d71153430a4
hayanLee <hayanlee.dev@gmail.com>
2024-07-31 19:35:35 +0900

    Fix : 메뉴바 로그아웃 이벤트핸들러 수정

--- src/app/(provider)/(root)/_components/Header/Header.tsx
+++ src/app/(provider)/(root)/_components/Header/Header.tsx
@@ -5,11 +5,14 @@ import Link from 'next/link';
 
 const Header = () => {
   const { logOutMutation } = useAuthMutation();
+  const handleLogout = () => {
+    logOutMutation();
+  };
   return (
     <div>
       <Link href="/">홈</Link>
       <Link href="/auth/log-in">로그인</Link>
-      <button onClick={logOutMutation}>로그아웃</button>
+      <button onClick={handleLogout}>로그아웃</button>
       <Link href="/auth/sign-up">회원가입</Link>
       <Link href="/search">검색</Link>
       <Link href="/cart">장바구니</Link>
