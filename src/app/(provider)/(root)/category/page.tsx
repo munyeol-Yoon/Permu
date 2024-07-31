c6d9ff1992349ef20ce47111fede90a837304334
muyeon <gmakin36@gmail.com>
2024-07-31 10:50:38 +0900

    Update: category-page 입력창 링크 추가

--- src/app/(provider)/(root)/category/page.tsx
+++ src/app/(provider)/(root)/category/page.tsx
@@ -1,5 +1,26 @@
+import SearchButtonSVG from '@@/public/searchButton.svg';
+import Link from 'next/link';
+
 const CategoryPage = () => {
-  return <div>CategoryPage</div>;
+  return (
+    <div>
+      <Link href={'/category/search'}>
+        <section className="flex justify-center items-center self-stretch">
+          <div className="relative w-full ml-11 mr-11">
+            <input
+              type="text"
+              placeholder="여름 시즌 추천템 20% 할인"
+              className="rounded-[4px] bg-[#b3b3b320] w-full px-2.5 py-0 pr-[60px] h-[42px] cursor-pointer"
+            />
+
+            <button className="absolute right-0 top-0 bottom-0 px-3 py-1">
+              <SearchButtonSVG />
+            </button>
+          </div>
+        </section>
+      </Link>
+    </div>
+  );
 };
 
 export default CategoryPage;
