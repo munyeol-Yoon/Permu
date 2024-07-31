679142639027a68b4f57cfe5ca455c6a083ca96b
박원빈 <harry21@kakao.com>
2024-07-31 14:49:25 +0900

    Update: CartItem 컴포넌트 내 선택 여부, 삭제 기능 추가

--- src/app/(provider)/(root)/cart/_components/CartItem.tsx
+++ src/app/(provider)/(root)/cart/_components/CartItem.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
+import { useAuth } from '@/contexts/auth.context/auth.context';
+import useCart from '@/hooks/useCart';
 import Image from 'next/image';
 
 interface CartItemProps {
@@ -7,18 +11,35 @@ interface CartItemProps {
 }
 
 const CartItem = ({ cartItem }: CartItemProps) => {
+  const { loggedUser } = useAuth();
+  const { deleteCartItem, updateCartItemSelected } = useCart();
+
+  const handleUpdateItemSelected = () => {
+    updateCartItemSelected(cartItem.productId, loggedUser!.id, !cartItem.isSelected);
+  };
+
+  const handleDeleteItem = () => {
+    deleteCartItem(cartItem.productId, loggedUser!.id);
+  };
+
   return (
     <li className="flex items-center px-5">
-      <Checkbox className="w-6 h-6 rounded-full data-[state=checked]:bg-[#0348FF] data-[state=checked]:text-white" />
+      <Checkbox
+        checked={cartItem.isSelected}
+        onClick={handleUpdateItemSelected}
+        className="w-6 h-6 rounded-full data-[state=checked]:bg-[#0348FF] data-[state=checked]:text-white"
+      />
       <div className="relative aspect-square max-w-[100px] h-[120px] mx-[33px]">
         <Image src={cartItem.Products.thumbNailURL} fill alt="" className="absolute" />
       </div>
       <div className="flex flex-col px-2.5 w-full">
         <div className="flex justify-between items-center">
           <p className="text-xs mb-1">{cartItem.Products.Brands.enName}</p>
-          <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
-            <path d="M13 0.500001L1 12.5M13 12.5L1 0.5" stroke="#231815" />
-          </svg>
+          <button onClick={handleDeleteItem}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
+              <path d="M13 0.500001L1 12.5M13 12.5L1 0.5" stroke="#231815" />
+            </svg>
+          </button>
         </div>
         <p className="font-semibold mb-2.5">{cartItem.Products.title}</p>
         <p className="text-xs text-[#B3B3B3] mb-1.5">옵션 : 옵션 A / 옵션 a / 옵션 1</p>
