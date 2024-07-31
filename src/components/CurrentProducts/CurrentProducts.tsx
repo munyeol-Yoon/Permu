3a0475e188c80a7e55d57e8fe84bed6bbf044088
KimDongSin <asstaff2020@gmail.com>
2024-07-31 15:45:15 +0900

    Chore: 현재 판매중인 상품 컴포넌트화

---
+++ src/components/CurrentProducts/CurrentProducts.tsx
@@ -0,0 +1,25 @@
+import CategoryMore from "@/components/CategoryMore";
+import ProductCard from "@/components/ProductCard";
+
+const CurrentProducts = () => {
+  return (
+    <div className="flex flex-col">
+      <CategoryMore title="현재 판매중인 상품" />
+      <div className="flex items-center justify-between">
+        {Array(3)
+          .fill(0)
+          .map((_, idx) => (
+            <ProductCard
+              key={idx} 
+              brand="브랜드명" 
+              name="제품명" 
+              discountPercentage={10} 
+              price={30000} 
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CurrentProducts;
