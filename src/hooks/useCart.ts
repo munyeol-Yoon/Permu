2521fa67d317c2208c7899c5d9a6400497789232
박원빈 <harry21@kakao.com>
2024-07-31 14:26:41 +0900

    Update: useCart 훅 업데이트 - useCallback을 사용하여 사용하는 컴포넌트에서의 불필요한 리렌더링을 최소화하였습니다.

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useCartsMutation } from './mutation';
 import { useCartsQuery } from './query';
 
@@ -6,30 +7,40 @@ const useCart = () => {
 
   const { addMutation, patchMutation, deleteMutation, deleteAllMutation } = useCartsMutation();
 
-  const addCartItem = async (productId: number, userId: string) => {
-    const { mutateAsync } = addMutation;
-    await mutateAsync({ productId, userId });
-  };
+  const addCartItem = useCallback(() => {
+    async (productId: number, userId: string) => {
+      const { mutateAsync } = addMutation;
+      await mutateAsync({ productId, userId });
+    };
+  }, [addMutation]);
 
-  const updateCartItemCount = async (productId: number, userId: string, count: number) => {
-    const { mutateAsync } = patchMutation;
-    await mutateAsync({ productId, userId, count });
-  };
+  const updateCartItemCount = useCallback(() => {
+    async (productId: number, userId: string, count: number) => {
+      const { mutateAsync } = patchMutation;
+      await mutateAsync({ productId, userId, count });
+    };
+  }, [patchMutation]);
 
-  const updateCartItemSelected = async (productId: number, userId: string, isSelected: boolean) => {
-    const { mutateAsync } = patchMutation;
-    await mutateAsync({ productId, userId, isSelected });
-  };
+  const updateCartItemSelected = useCallback(() => {
+    async (productId: number, userId: string, isSelected: boolean) => {
+      const { mutateAsync } = patchMutation;
+      await mutateAsync({ productId, userId, isSelected });
+    };
+  }, [patchMutation]);
 
-  const deleteCartItem = async (productId: number, userId: string) => {
-    const { mutateAsync } = deleteMutation;
-    await mutateAsync({ productId, userId });
-  };
+  const deleteCartItem = useCallback(() => {
+    async (productId: number, userId: string) => {
+      const { mutateAsync } = deleteMutation;
+      await mutateAsync({ productId, userId });
+    };
+  }, [deleteMutation]);
 
-  const deleteAllCartItem = async (userId: string) => {
-    const { mutateAsync } = deleteAllMutation;
-    await mutateAsync({ userId });
-  };
+  const deleteAllCartItem = useCallback(() => {
+    async (userId: string) => {
+      const { mutateAsync } = deleteAllMutation;
+      await mutateAsync({ userId });
+    };
+  }, [deleteAllMutation]);
 
   return { cartList, addCartItem, updateCartItemCount, updateCartItemSelected, deleteCartItem, deleteAllCartItem };
 };
