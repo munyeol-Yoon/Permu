c2dc42db6cf147a4380db08d731de5d8709dabfb
leeejin <asd72621@naver.com>
2024-08-01 14:10:58 +0900

    Design:데스크탑 기준 디자인 수정

--- src/app/(provider)/(root)/layout.tsx
+++ src/app/(provider)/(root)/layout.tsx
@@ -10,7 +10,7 @@ const RootLayout = ({ children }: PropsWithChildren) => {
   const isCategoryPage = pathname.startsWith('/category');
 
   return (
-    <div className="w-screen container grid grid-cols-1 md:grid-cols-2">
+    <div className="w-screen h-screen container grid grid-cols-1 md:grid-cols-2">
       <div className="relative">
         {/* <Image src={EventBenner} width={400} height={800} alt="이벤트" className="absolute object-cover right-0" /> */}
       </div>
