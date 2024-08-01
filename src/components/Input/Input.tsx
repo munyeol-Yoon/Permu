c274e87cfba06c4b3a91bfc36c4803bb39505901
leeejin <asd72621@naver.com>
2024-08-01 10:58:29 +0900

    Fix : 회의결과 후자로 결정 - 필요할때마다 global.css에있는 스타일적용

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -8,70 +8,17 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   placeholder?: string;
 };
 
-// 일반적으로 쓰는 Input 안에 border 다 있는거
+// 일반적으로 쓰는 Input 안에 border 다 있는거 className 안넣으면 기본 shadcn스타일인 input이 나옴니다
 export const DefaultInput = forwardRef<HTMLInputElement, InputProps>(
-  ({ type = 'text', label, className, placeholder, ...props }, ref) => {
+  ({ type = 'text', label, className = 'input-default', placeholder, ...props }, ref) => {
     const id = useId();
     return (
       <>
         {label && <label htmlFor={id}>{label}</label>}
-        <Input
-          type={type}
-          id={id}
-          className={'bg-background border border-input' + className}
-          placeholder={placeholder}
-          ref={ref}
-          {...props}
-        />
+        <Input type={type} id={id} className={className} placeholder={placeholder} ref={ref} {...props} />
       </>
-    );
-  }
-);
-
-// Search할 때 쓰는 Input 배경색은 그레이임
-export const SearchInput = forwardRef<HTMLInputElement, InputProps>(
-  ({ type = 'text', label, className, placeholder, ...props }, ref) => {
-    const id = useId();
-    return (
-      <>
-        {label && <label htmlFor={id}>{label}</label>}
-        <Input
-          type={type}
-          id={id}
-          className={'bg-gray-500 border-none' + className}
-          placeholder={placeholder}
-          ref={ref}
-          {...props}
-        />
-      </>
-    );
-  }
-);
-
-// BorderInput 밑에만 border있는거
-export const BorderInput = forwardRef<HTMLInputElement, InputProps>(
-  ({ type = 'text', label, className, placeholder, ...props }, ref) => {
-    const id = useId();
-    return (
-      <div className="w-full flex gap-[30px] items-center">
-        {label && (
-          <label className="w-1/4" htmlFor={id}>
-            {label}
-          </label>
-        )}
-        <Input
-          type={type}
-          id={id}
-          className={'border-b-[1.5px] px-[40px] py-[20px] grow' + className}
-          placeholder={placeholder}
-          ref={ref}
-          {...props}
-        />
-      </div>
     );
   }
 );
 
 DefaultInput.displayName = 'DefaultInput';
-SearchInput.displayName = 'SearchInput';
-BorderInput.displayName = 'BorderInput';
