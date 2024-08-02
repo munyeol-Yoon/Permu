1fcb1e1ff98d13ba69165f0db65bf93ab290ddca
박원빈 <harry21@kakao.com>
2024-08-02 21:25:45 +0900

    Fix: 빌드 에러

--- src/app/(provider)/(root)/category/search/result/page.tsx
+++ src/app/(provider)/(root)/category/search/result/page.tsx
@@ -1,30 +1,34 @@
 'use client';
+
 import ProductCard from '@/components/ProductCard';
 import { useSearchQuery } from '@/hooks/query';
 import { Product } from '@/types/products';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-const ResultPage = () => {
+const ResultPageContent = () => {
   const searchParams = useSearchParams();
-  const { data, isPending, error } = useSearchQuery(searchParams.get('query') || '');
+  const query = searchParams.get('query');
+  const { data, isPending, error } = useSearchQuery(query || '');
 
   if (isPending) return;
-
-  if (error) console.log(error);
-
-  console.log(data)
 
   return (
     <div className="container mx-auto p-5">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data && data.data.map((productItem: Product) => (
-          <ProductCard
-            key={productItem.productId}
-            product={productItem}
-          />
+        {data.data.map((productItem: Product) => (
+          <ProductCard key={productItem.productId} product={productItem} />
         ))}
       </div>
     </div>
+  );
+};
+
+const ResultPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResultPageContent />
+    </Suspense>
   );
 };
 
