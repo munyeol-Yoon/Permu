70fccfcbd7589bf19fa6ddb2870fc74470d01897
leeejin <asd72621@naver.com>
2024-08-02 16:27:31 +0900

    Fix :빌드에러 확인

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -7,19 +7,16 @@ export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
     const option = searchParams.get('option') as string;
 
-    switch (option) {
-      case 'popular':
-        break;
-      case 'recent':
-        const { data: RecentData } = await supabase
-          .from('Products')
-          .select('*')
-          .order('createdAt', { ascending: true })
-          .limit(3);
-        return NextResponse.json(RecentData);
-      default:
-        const { data: DefaultData } = await supabase.from('Products').select('*').limit(3);
-        return NextResponse.json(DefaultData);
+    if (option === 'recent') {
+      const { data: RecentData } = await supabase
+        .from('Products')
+        .select('*')
+        .order('createdAt', { ascending: true })
+        .limit(3);
+      return NextResponse.json(RecentData);
+    } else {
+      const { data: DefaultData } = await supabase.from('Products').select('*').limit(3);
+      return NextResponse.json(DefaultData);
     }
   } catch (error) {
     return NextResponse.json({ error });
