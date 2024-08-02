1e99b0e05aa45a2f553acd462e936e69e5200cc3
leeejin <asd72621@naver.com>
2024-08-02 15:42:20 +0900

    Fix : 빌드 에러 확인

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -17,10 +17,9 @@ export async function GET(request: NextRequest) {
           .order('createdAt', { ascending: true })
           .limit(3);
         return NextResponse.json(RecentData || []);
-      default:
-        const { data: DefaultData } = await supabase.from('Products').select('*').limit(3);
-        return NextResponse.json(DefaultData || []);
     }
+    const { data: DefaultData } = await supabase.from('Products').select('*').limit(3);
+    return NextResponse.json(DefaultData || []);
   } catch (error) {
     return NextResponse.json({ error });
   }
