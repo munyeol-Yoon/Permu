b3808dead0890e4307f5c77921595c98a1e82d27
leeejin <asd72621@naver.com>
2024-08-03 19:47:02 +0900

    Refactor: Products 불러오는 함수 리펙토링

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -7,33 +7,17 @@ export async function GET(request: NextRequest) {
     const supabase = createClient();
     const { searchParams } = new URL(request.url);
     const option = searchParams.get('option') as string;
-
-    if (option === 'recent') {
-      const { data: recentData } = await supabase.from('Products').select('*').limit(3);
-
-      const brandIds = recentData?.map((product) => product.brandId) || [];
-      const { data: brands } = await supabase.from('Brands').select('*').in('brandId', brandIds);
-
-      const brandMap = new Map(brands?.map((brand) => [brand.brandId, brand]));
-
-      const productWithDiscountedPrice: Product[] =
-        recentData?.map((data) => ({
-          ...data,
-          Brand: brandMap.get(data.brandId) || null,
-          discountedPrice: data.price - (data.price * data.discount) / 100
-        })) || [];
-
-      return NextResponse.json(productWithDiscountedPrice);
-    }
-    const { data: defaultData } = await supabase.from('Products').select('*').limit(3);
-
-    const brandIds = defaultData?.map((product) => product.brandId) || [];
+    const { data } =
+      option === 'recent'
+        ? await supabase.from('Products').select('*').order('createdAt', { ascending: true }).limit(3)
+        : await supabase.from('Products').select('*').limit(3);
+    const brandIds = data?.map((product) => product.brandId) || [];
     const { data: brands } = await supabase.from('Brands').select('*').in('brandId', brandIds);
 
     const brandMap = new Map(brands?.map((brand) => [brand.brandId, brand]));
 
     const productWithDiscountedPrice: Product[] =
-      defaultData?.map((data) => ({
+      data?.map((data) => ({
         ...data,
         Brand: brandMap.get(data.brandId) || null,
         discountedPrice: data.price - (data.price * data.discount) / 100
