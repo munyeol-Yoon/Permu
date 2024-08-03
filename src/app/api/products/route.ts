2f09d05fe5f00028373da179a0f86562c42a3237
leeejin <asd72621@naver.com>
2024-08-03 19:41:28 +0900

    Fix : 빌드 에러 확인

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,23 +2,6 @@ import { createClient } from '@/supabase/server';
 import { Product } from '@/types/products';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function fetchProducts(data: any[] | null) {
-  const supabase = createClient();
-
-  const brandIds = data?.map((product) => product.brandId) || [];
-  const { data: brands } = await supabase.from('Brands').select('*').in('brandId', brandIds);
-
-  const brandMap = new Map(brands?.map((brand) => [brand.brandId, brand]));
-
-  const productWithDiscountedPrice: Product[] =
-    data?.map((data) => ({
-      ...data,
-      Brand: brandMap.get(data.brandId) || null,
-      discountedPrice: data.price - (data.price * data.discount) / 100
-    })) || [];
-  return productWithDiscountedPrice;
-}
-
 export async function GET(request: NextRequest) {
   try {
     const supabase = createClient();
@@ -28,13 +11,35 @@ export async function GET(request: NextRequest) {
     if (option === 'recent') {
       const { data: recentData } = await supabase.from('Products').select('*').limit(3);
 
-      const products = await fetchProducts(recentData);
-      return NextResponse.json(products);
+      const brandIds = recentData?.map((product) => product.brandId) || [];
+      const { data: brands } = await supabase.from('Brands').select('*').in('brandId', brandIds);
+
+      const brandMap = new Map(brands?.map((brand) => [brand.brandId, brand]));
+
+      const productWithDiscountedPrice: Product[] =
+        recentData?.map((data) => ({
+          ...data,
+          Brand: brandMap.get(data.brandId) || null,
+          discountedPrice: data.price - (data.price * data.discount) / 100
+        })) || [];
+
+      return NextResponse.json(productWithDiscountedPrice);
     }
     const { data: defaultData } = await supabase.from('Products').select('*').limit(3);
 
-    const products = await fetchProducts(defaultData);
-    return NextResponse.json(products);
+    const brandIds = defaultData?.map((product) => product.brandId) || [];
+    const { data: brands } = await supabase.from('Brands').select('*').in('brandId', brandIds);
+
+    const brandMap = new Map(brands?.map((brand) => [brand.brandId, brand]));
+
+    const productWithDiscountedPrice: Product[] =
+      defaultData?.map((data) => ({
+        ...data,
+        Brand: brandMap.get(data.brandId) || null,
+        discountedPrice: data.price - (data.price * data.discount) / 100
+      })) || [];
+
+    return NextResponse.json(productWithDiscountedPrice);
   } catch (error) {
     return NextResponse.json({ error });
   }
