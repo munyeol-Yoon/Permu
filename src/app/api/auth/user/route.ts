4dd7ad81cee25abcfe13e0b0722e91457a0d6778
hayanLee <hayanlee.dev@gmail.com>
2024-08-04 23:57:11 +0900

    Fix : 필수 입력란 표시
    - Email rate limit exceeded으로 콘솔 찍어놓음

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -30,6 +30,7 @@ export async function PATCH(request: NextRequest) {
   try {
     const { email, password, ...res } = await request.json();
     const supabase = createClient();
+
     const {
       data: { user },
       error: userError
@@ -38,11 +39,11 @@ export async function PATCH(request: NextRequest) {
     if (userError) throw new Error(userError.message);
     if (!user) throw new Error('User notFound');
 
-    // // 비밀번호 업데이트
-    const { data: passwordUpdate, error: passwordError } = await supabase.auth.updateUser({ password });
+    // 비밀번호 업데이트
+    const { error: passwordError } = await supabase.auth.updateUser({ password });
     if (passwordError) throw new Error(passwordError.message);
 
-    // //db 업데이트
+    // db 업데이트
     const { data: infoUpdate, error: infoUpdateError } = await supabase
       .from('Users')
       .update({ ...res, isNew: false })
