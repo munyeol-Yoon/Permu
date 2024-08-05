e086e2205f6e7bce4e3b917ce5fb11ac242b49d3
hayanLee <hayanlee.dev@gmail.com>
2024-08-06 00:24:44 +0900

    Design : 메인 페이지 헤더 애니메이션 구현
    - 조건문을 isProductOrHomePage 변수로 뺌
    - 스크롤 이벤트 리스너 등록

--- src/app/(provider)/(root)/layout.tsx
+++ src/app/(provider)/(root)/layout.tsx
@@ -2,23 +2,48 @@
 
 import SearchHeader from '@/components/SearchPage/SearchHeader';
 import { usePathname } from 'next/navigation';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useCallback, useEffect, useState } from 'react';
 import Header from './_components/Header';
 import TopBanner from './_components/Header/_components/TopBanner';
 
 const RootLayout = ({ children }: PropsWithChildren) => {
   const pathname = usePathname();
   const isCategoryPage = pathname.startsWith('/category');
-  const isProductPage = pathname.startsWith('/products');
-  const isHomePage = pathname === '/';
+  const isProductOrHomePage = pathname.startsWith('/products') || pathname === '/';
+
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  const handleScroll = useCallback(() => {
+    setIsScrolled(window.scrollY > 0);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll]);
 
   return (
-    <div className="w-screen h-screen container grid mx-auto">
-      <div className="bg-white max-w-[600px] min-w-[600px] h-full mx-auto flex flex-col">
-        <TopBanner />
-        <div className="sticky top-0 z-10">
-          {isCategoryPage ? <SearchHeader /> : (isProductPage || isHomePage) && <Header />}
-        </div>
+    <div className="container mx-auto min-h-screen">
+      <div className="bg-white w-full max-w-[600px] mx-auto flex flex-col">
+        {isCategoryPage ? (
+          <SearchHeader />
+        ) : (
+          isProductOrHomePage && (
+            <>
+              <div
+                className={`
+                duration-300 overflow-hidden
+                ${isScrolled ? 'max-h-0 opacity-0' : 'max-h-[50px] opacity-100'}
+              `}
+              >
+                <TopBanner />
+              </div>
+              <div className="sticky top-0 z-10">
+                <Header />
+              </div>
+            </>
+          )
+        )}
         {children}
       </div>
     </div>
