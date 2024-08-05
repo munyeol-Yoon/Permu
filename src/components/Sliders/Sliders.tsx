a9ae0910f55c8757619c70c9aad71142ed870789
leeejin <asd72621@naver.com>
2024-08-05 20:32:37 +0900

    Fix : 빌드 에러 확인

--- src/components/Sliders/Sliders.tsx
+++ src/components/Sliders/Sliders.tsx
@@ -23,9 +23,9 @@ const Sliders = ({ data, count }: SlidersProps) => {
           <CarouselItem className="flex-row-10" key={index}>
             {brandChunk.map((data) =>
               'productId' in data ? (
-                <ProductCard key={data.productId} product={data} />
+                <ProductCard key={data.productId} product={data as Product} />
               ) : (
-                <CategoryCard key={data.brandId} brand={data} />
+                <CategoryCard key={data.brandId} brand={data as Tables<'Brands'>} />
               )
             )}
           </CarouselItem>
