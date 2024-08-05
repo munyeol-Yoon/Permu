99b82b3d1edee9f1014c3c4e7b9280b257838552
hayanLee <hayanlee.dev@gmail.com>
2024-08-05 23:28:30 +0900

    Design : Footer 디자인 수정

--- src/app/(provider)/(root)/_components/Footer/Footer.tsx
+++ src/app/(provider)/(root)/_components/Footer/Footer.tsx
@@ -1,21 +1,35 @@
 import Toggle from '@/components/Toggle';
 import { Accordion } from '@/components/ui/accordion';
-import FooterCallInfo from './FooterCallInfo';
-import FooterSNS from './FooterSNS';
+import CallInfo from './_components/CallInfo';
+import SNSInfo from './_components/SNSInfo';
 
 const Footer = () => {
   return (
-    <Accordion type="single">
-      <Toggle trigger="사업자 정보">몰라</Toggle>
-      <Toggle trigger="법적고지사항">모른다</Toggle>
-      <Toggle trigger="고객지원">
-        <div className="grid grid-cols-[300px_1px_300px] grid-rows-[269px]">
-          <FooterCallInfo />
-          <div className="bg-gray-500" />
-          <FooterSNS />
-        </div>
-      </Toggle>
-    </Accordion>
+    <>
+      <Accordion type="single" collapsible>
+        <Toggle trigger="사업자 정보" className="px-10 text-xl font-thin">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus nisi provident porro repellendus suscipit
+          consectetur dolor itaque doloribus neque numquam, accusantium in reprehenderit modi, accusamus asperiores
+          excepturi! Dolore, voluptatum saepe!
+        </Toggle>
+        <Toggle trigger="법적고지사항" className="px-10 text-xl font-thin">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, esse sit quidem molestias ipsam doloribus
+          perspiciatis reprehenderit, consequatur repellendus incidunt distinctio, ducimus quo. Numquam commodi autem
+          vitae voluptatem fugiat. Animi!
+        </Toggle>
+        <Toggle trigger="고객지원" className="px-10 text-xl font-thin">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, esse sit quidem molestias ipsam doloribus
+          perspiciatis reprehenderit, consequatur repellendus incidunt distinctio, ducimus quo. Numquam commodi autem
+          vitae voluptatem fugiat. Animi!
+        </Toggle>
+      </Accordion>
+
+      <div className="grid grid-cols-[300px_1px_300px] grid-rows-[269px]">
+        <CallInfo />
+        <div className="bg-muted" />
+        <SNSInfo />
+      </div>
+    </>
   );
 };
 
---
+++ src/app/(provider)/(root)/_components/Footer/_components/CallInfo.tsx
@@ -0,0 +1,22 @@
+import { Button } from '@/components/ui/button';
+
+const CallInfo = () => {
+  return (
+    <div className="flex flex-col items-center justify-between py-5">
+      <div className="flex flex-col items-center">
+        <p>CS CENTER</p>
+        <p className="underline">010-4188-1824</p>
+      </div>
+      <div className="flex flex-col items-center font-medium">
+        <p>상담시간 AM10:00 ~ PM:06:00</p>
+        <p>점심시간 PM12:00 ~ PM01:00</p>
+      </div>
+
+      <p>휴일 및 공휴일 휴무</p>
+
+      <Button>고객센터 전화 연결</Button>
+    </div>
+  );
+};
+
+export default CallInfo;
