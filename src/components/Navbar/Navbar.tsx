9bba865c8010ea5a61bbff283672b1fda06a24fb
hayanLee <hayanlee.dev@gmail.com>
2024-08-06 18:51:07 +0900

    Fix : Navbar cursor 적용

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -12,13 +12,13 @@ const Navbar = ({ title, href, isHome }: NavbarProps) => {
   return (
     <div className="flex items-center relative p-5 text-[20px] tracking-[6px] justify-between">
       <Link href={href}>
-        <BackButton />
+        <BackButton className="cursor-pointer" />
       </Link>
       <h1 className="text-center grow">{title}</h1>
 
       {isHome ? (
-        <Link href={HOME} className="ml-auto">
-          <Home />
+        <Link href={HOME} className="ml-auto ">
+          <Home className="cursor-pointer" />
         </Link>
       ) : (
         <div className="w-5"></div>
