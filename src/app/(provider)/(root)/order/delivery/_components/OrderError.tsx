04bca6de404dc3b629112801dc66e41e618bd8d8
박원빈 <harry21@kakao.com>
2024-08-06 17:59:58 +0900

    Design: 주문 실패 페이지 링크 추가

--- src/app/(provider)/(root)/order/delivery/_components/OrderError.tsx
+++ src/app/(provider)/(root)/order/delivery/_components/OrderError.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar';
+import Link from 'next/link';
 
 const OrderError = () => {
   return (
@@ -8,6 +9,7 @@ const OrderError = () => {
       <div className="max-w-[600px] h-full flex flex-col items-center justify-center">
         <div className="h-full flex flex-col justify-center items-center gap-9">
           <p className="text-[20px] font-bold">주문에 실패하였습니다.</p>
+          <Link href="/cart">장바구니로 돌아가기</Link>
         </div>
       </div>
     </>
