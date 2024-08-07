cc8403720cb34900e2690071fd23aaf777b65dc9
hayanLee <hayanlee.dev@gmail.com>
2024-08-07 17:38:00 +0900

    Design : InfoCard Link 연결

--- src/app/(provider)/(root)/mypage/_components/InfoCard/InfoCard.tsx
+++ src/app/(provider)/(root)/mypage/_components/InfoCard/InfoCard.tsx
@@ -1,13 +1,26 @@
 import { cn } from '@/utils/cn';
+import Link from 'next/link';
 import { PropsWithChildren } from 'react';
 
 interface InfoCardProps {
   title: string;
+  href?: string;
   sm?: boolean;
   className?: string;
 }
-const InfoCard = ({ title, children, sm, className }: PropsWithChildren<InfoCardProps>) => {
-  return (
+const InfoCard = ({ title, href, children, sm, className }: PropsWithChildren<InfoCardProps>) => {
+  const content = (
+    <>
+      <p className="text-base">{title}</p>
+      <p className={cn('text-xl text-accent font-semibold', { 'text-lg font-bold': sm })}>{children}</p>
+    </>
+  );
+
+  return href ? (
+    <Link href={href} className={cn('flex-1 px-4 py-5 bg-white rounded flex flex-col', className)}>
+      {content}
+    </Link>
+  ) : (
     <div
       className={cn(
         'flex-1 px-4 py-5 bg-white rounded flex flex-col',
@@ -17,8 +30,7 @@ const InfoCard = ({ title, children, sm, className }: PropsWithChildren<InfoCard
         className
       )}
     >
-      <p className="text-base">{title}</p>
-      <p className={cn('text-xl text-accent font-semibold', { 'text-lg font-bold': sm })}>{children}</p>
+      {content}
     </div>
   );
 };
