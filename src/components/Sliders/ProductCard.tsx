587552afea96f9fd7622b80d278f90ddf5ae3cbf
leeejin <asd72621@naver.com>
2024-08-07 11:03:10 +0900

    Design : 피드백 반영 디자인 수정

--- src/components/Sliders/ProductCard.tsx
+++ src/components/Sliders/ProductCard.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { Product } from '@/types/products';
+import WishSVG from '@@/public/heart/wish-icon.svg';
 import Image from 'next/image';
 import Link from 'next/link';
-
 export interface ProductProps {
   product: Product;
 }
@@ -12,30 +12,36 @@ const ProductCard = ({ product }: ProductProps) => {
   const discount = product.discount ?? 0;
   const discountAmount = price * (discount / 100);
   const resultPrice = price - discountAmount;
+  const title = product.title ?? '';
 
   return (
-    <div className="w-[180px] flex flex-col gap-1">
+    <div className="w-[180px] flex flex-col">
       <Link href={`/products/${product.productId}`}>
-        <Image
-          src={product.thumbNailURL || ''}
-          width={200}
-          height={200}
-          alt={product.title || ''}
-          className="w-full h-[200px] object-contain rounded"
-          unoptimized
-        />
+        <div className="relative">
+          <Image
+            src={product.thumbNailURL || ''}
+            width={200}
+            height={200}
+            alt={title}
+            className="w-full h-[200px] object-contain"
+            unoptimized
+          />
+          <WishSVG className="absolute bottom-2 right-2" />
+        </div>
       </Link>
-      <span className="text-[12px] line-clamp-1">{product.Brand?.krName}</span>
-      <p className="font-semibold line-clamp-1">{product.title || ''}</p>
-      <div className="flex justify-between items-center">
-        <span className="font-semibold">{(product.discountedPrice || resultPrice || 0).toLocaleString()}원</span>
-        &nbsp;
-        {(product.discount || 0) > 0 && (
-          <>
-            <span className="flex-1 text-gray-500 text-sm line-through">{product.price?.toLocaleString()}원</span>
-            <p className="text-semibold text-[#FF0000]">{product.discount}%</p>
-          </>
-        )}
+      <div className="flex flex-col gap-1">
+        <span className="text-[12px] line-clamp-1">{product.Brand?.krName}</span>
+        <p className="font-semibold line-clamp-1">{title.length > 7 ? title.slice(0, 7) + '...' : title}</p>
+        <div className="flex justify-between items-center">
+          <span className="font-semibold">{(product.discountedPrice || resultPrice || 0).toLocaleString()}원</span>
+          &nbsp;
+          {(product.discount || 0) > 0 && (
+            <>
+              <span className="flex-1 text-gray-500 text-xs line-through">{product.price?.toLocaleString()}원</span>
+              <p className="font-bold text-[#FF0000]">{product.discount}%</p>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
