72c318a11c376242e80c9da17187cd1727cd6535
hayanLee <hayanlee.dev@gmail.com>
2024-08-07 16:59:28 +0900

    Design : 리뷰 페이지 UI, OrderCard UI 수정

--- src/app/(provider)/(root)/mypage/orders/_components/OrderCard/OrderCard.tsx
+++ src/app/(provider)/(root)/mypage/orders/_components/OrderCard/OrderCard.tsx
@@ -1,17 +1,22 @@
 import { AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { Button } from '@/components/ui/button';
+import useAlert from '@/hooks/useAlert';
 import { MyOrder } from '@/types/myPage/order';
 
 import { formatDate } from '@/utils/format';
-import Image from 'next/image';
+import OrderItem from '../OrderItem';
 
 interface OrderCardProps {
   order: MyOrder;
+  review?: boolean;
 }
 
-const OrderCard = ({ order }: OrderCardProps) => {
+const OrderCard = ({ order, review }: OrderCardProps) => {
+  const { showInfoAlert } = useAlert();
   const { orderId, OrdersDetail } = order;
   const formattedDate = formatDate(order.createAt);
+  const handleClick = () => showInfoAlert('준비중입니다!');
+
   return (
     <AccordionItem value={crypto.randomUUID()}>
       <AccordionTrigger>
@@ -20,29 +25,30 @@ const OrderCard = ({ order }: OrderCardProps) => {
           <span className="text-sm text-muted">주문번호 {orderId.split('-')[0]}</span>
         </div>
       </AccordionTrigger>
+
       <AccordionContent>
         {OrdersDetail.map((orderItem) => {
           const { title, thumbNailURL } = orderItem.Products;
           return (
-            <div className="flex gap-x-5 mb-3" key={orderItem.productId}>
-              <div className="w-[100px] h-[100px] aspect-square relative">
-                <Image src={thumbNailURL || ''} alt={title || ''} className="object-cover" fill />
-              </div>
-              <div className="flex flex-col gap-y-2.5">
-                <p>구매일 : {formattedDate}</p>
-                <p className="text-base font-semibold line-clamp-1">{title}</p>
-                <p className="text-muted">옵션 :</p>
-              </div>
+            <div key={orderItem.productId} className="flex flex-col mb-3">
+              <OrderItem title={title} thumbNailURL={thumbNailURL} date={formattedDate} />
+              {review ? (
+                <Button size="sm" onClick={handleClick} className="mt-6">
+                  리뷰 작성하기
+                </Button>
+              ) : null}
             </div>
           );
         })}
 
-        <div className="flex flex-col bg-teal-50">
-          <Button variant="outline" className="my-0">
-            교환/환불내역
-          </Button>
-          <Button className="my-2">배송조회</Button>
-        </div>
+        {!review && (
+          <div className="flex flex-col bg-teal-50">
+            <Button variant="outline" className="my-0">
+              교환/환불내역
+            </Button>
+            <Button className="my-2">배송조회</Button>
+          </div>
+        )}
       </AccordionContent>
     </AccordionItem>
   );
--- src/app/(provider)/(root)/mypage/review/page.tsx
+++ src/app/(provider)/(root)/mypage/review/page.tsx
@@ -1,5 +1,44 @@
+'use client';
+
+import Loading from '@/components/Loading';
+import Navbar from '@/components/Navbar';
+import { Accordion } from '@/components/ui/accordion';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import useOrderListQuery from '@/hooks/query/useOrderListQuery';
+import { MyOrder } from '@/types/myPage/order';
+import OrderCard from '../orders/_components/OrderCard';
 const ReviewPage = () => {
-  return <div>ReviewPage</div>;
+  const { data: OrderList, isPending } = useOrderListQuery();
+  if (isPending) return <Loading />;
+
+  return (
+    <div>
+      <Navbar title="리뷰" isHome />
+      <Tabs defaultValue="writable" className="flex flex-col mx-[50px]">
+        <TabsList>
+          <TabsTrigger value="writable" className="px-10">
+            지금 작성 가능한 리뷰
+          </TabsTrigger>
+          <TabsTrigger value="wrote" className="px-10">
+            내가 작성한 리뷰
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="writable">
+          <Accordion type="single" collapsible>
+            {OrderList?.map((order: MyOrder) => <OrderCard key={order.orderId} order={order} review />)}
+          </Accordion>
+        </TabsContent>
+
+        <TabsContent value="wrote">
+          <Accordion type="single" collapsible>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi laboriosam minima culpa magni fuga natus
+            neque veritatis officiis labore, fugiat sapiente quam, dolorem eligendi tenetur, commodi soluta nisi
+            recusandae architecto.
+          </Accordion>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default ReviewPage;
