4146f451cd94b7dbce338352ed069426e432bc73
leeejin <asd72621@naver.com>
2024-08-07 17:24:03 +0900

    Update : 리뷰 컴포넌트 세팅

--- src/app/(provider)/(root)/products/[productId]/review/page.tsx
+++ src/app/(provider)/(root)/products/[productId]/review/page.tsx
@@ -1,4 +1,10 @@
+import Navbar from '@/components/Navbar';
+
 const ReviewPage = () => {
-  return <div>리뷰</div>;
+  return (
+    <div className="relative max-w-[600px] flex flex-col h-full">
+      <Navbar title="전체후기사진 모아보기" isHome />
+    </div>
+  );
 };
 export default ReviewPage;
