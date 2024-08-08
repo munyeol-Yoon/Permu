bdee5a2cbdfaec7c808c9e23418388cc29f4aea6
hayanLee <hayanlee.dev@gmail.com>
2024-08-08 12:04:29 +0900

    Design : 페이지 레이아웃 추가

---
+++ src/app/(provider)/(root)/(mainHeader)/layout.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { cn } from '@/utils/cn';
+import { PropsWithChildren, useCallback, useEffect, useState } from 'react';
+import Header from '../_components/Header';
+import TopBanner from '../_components/Header/_components/TopBanner';
+
+const RootLayout = ({ children }: PropsWithChildren) => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  const handleScroll = useCallback(() => {
+    setIsScrolled(window.scrollY > 0);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll]);
+
+  return (
+    <>
+      <div
+        className={cn('transition-all duration-300 overflow-hidden', {
+          'max-h-0 opacity-0': isScrolled,
+          'max-h-[50px] opacity-100': !isScrolled
+        })}
+      >
+        <TopBanner />
+      </div>
+      <div className="sticky top-0 z-10">
+        <Header />
+      </div>
+      {children}
+    </>
+  );
+};
+
+export default RootLayout;
