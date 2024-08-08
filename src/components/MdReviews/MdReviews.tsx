6af73cd9b545366bb85718355a40b43b6850ab48
hayanLee <hayanlee.dev@gmail.com>
2024-08-08 10:14:52 +0900

    Design : 캐러셀 auto play 적용, HeaderNav/banner hover,active 스타일링

--- src/components/MdReviews/MdReviews.tsx
+++ src/components/MdReviews/MdReviews.tsx
@@ -1,12 +1,21 @@
+'use client';
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
 import MockReview from '@/mockup/mdPick.json';
+import Autoplay from 'embla-carousel-autoplay';
 import ReviewCard from '../ReviewCard';
 
 const MdReviews = () => {
   return (
     <div className="flex flex-col px-5">
       <h3 className="text-xl font-bold my-4">MD pick - 이달의 제품 추천 리뷰</h3>
-      <Carousel>
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 4000,
+            stopOnInteraction: false
+          })
+        ]}
+      >
         <CarouselContent>
           {MockReview.map((review) => (
             <CarouselItem className="basis-2/3" key={review.reviewId}>
