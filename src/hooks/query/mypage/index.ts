75e29481d1b77890860dcf0db3e71c3ca97d3843
hayanLee <hayanlee.dev@gmail.com>
2024-08-12 12:45:42 +0900

    Fix : 빌드 에러

--- src/hooks/query/mypage/index.ts
+++ src/hooks/query/mypage/index.ts
@@ -1,4 +1,3 @@
 export { default as useCouponQuery } from './useCouponQuery';
 export { default as useOrderListQuery } from './useOrderListQuery';
-export { default as useUserResourceQuery } from './useUserResourceQuery';
 export { default as useUserWishesQuery } from './useUserWishesQuery';
