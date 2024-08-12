3483430de91df54bf06b6ec1bebc2726dc547598
hayanLee <hayanlee.dev@gmail.com>
2024-08-12 16:13:33 +0900

    Fix : account-form, email-confirm UI 개선

--- src/app/(provider)/(root)/auth/sign-up/account-form/page.tsx
+++ src/app/(provider)/(root)/auth/sign-up/account-form/page.tsx
@@ -1,16 +1,19 @@
 'use client';
+import Input from '@/components/Input';
+import Loading from '@/components/Loading';
 import Navbar from '@/components/Navbar';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { AUTH_SIGN_UP_EMAIL_CONFIRM_PATHNAME } from '@/constant/pathname';
-import { useAuth } from '@/contexts/auth.context/auth.context';
 import { useAuthMutation } from '@/hooks/mutation';
+import useAuthQuery from '@/hooks/query/useAuthQuery';
 import useAlert from '@/hooks/useAlert';
 import { validateForm, validatePhoneNumber, ValidationInputProps } from '@/utils/validateCheck';
 import Link from 'next/link';
 import { useRef } from 'react';
 const AccountForm = () => {
   const { showAlert } = useAlert();
+
   const passwordRef = useRef<HTMLInputElement>(null);
   const passwordCheckRef = useRef<HTMLInputElement>(null);
   const nameRef = useRef<HTMLInputElement>(null);
@@ -18,11 +21,12 @@ const AccountForm = () => {
   const genderRef = useRef<HTMLInputElement>(null);
   const phoneRef = useRef<HTMLInputElement>(null);
 
-  const { loggedUser } = useAuth();
+  const { data: loggedUser, isPending } = useAuthQuery();
   const { userInfoMutation } = useAuthMutation();
   const { showWarningAlert } = useAlert();
 
-  if (!loggedUser) return <div>로그인한 유저 없음 로그인 필요</div>;
+  if (!loggedUser) return <div>로그인 유저 없음</div>;
+  if (isPending) return <Loading />;
   const {
     userData: { email }
   } = loggedUser;
@@ -90,55 +94,53 @@ const AccountForm = () => {
           <h3 className="py-5 border-b">기본정보</h3>
           <div className="flex items-center">
             <label htmlFor="email" className="w-1/4">
-              <span className="text-blue-500">*</span>
+              <span className="text-accent">*</span>
               아이디
             </label>
-            <input
+            <Input
+              variant="underline"
               type="email"
               id="email"
-              className="border-b px-[40px] py-4 text-center grow"
-              placeholder="아이디를 입력해 주세요."
+              placeholder={loggedUser.email || '아이디를 입력해 주세요.'}
               disabled
+              className="grow"
             />
           </div>
+
           <div className="flex items-center">
             <label htmlFor="password" className="w-1/4">
-              <span className="text-blue-500">*</span>
+              <span className="text-accent">*</span>
               비밀번호
             </label>
-            <input
+            <Input
+              variant="underline"
               type="password"
               id="password"
-              className="border-b px-[40px] py-4 text-center grow"
               placeholder="비밀번호를 입력해 주세요."
               ref={passwordRef}
+              className="grow"
             />
           </div>
           <div className="flex items-center">
             <label htmlFor="password-check" className="w-1/4">
-              <span className="text-blue-500">*</span>
+              <span className="text-accent">*</span>
               비밀번호 확인
             </label>
-            <input
+            <Input
+              variant="underline"
               type="password"
               id="password-check"
-              className="border-b px-[40px] py-4 text-center grow"
               placeholder="비밀번호 확인을 입력해 주세요."
               ref={passwordCheckRef}
+              className="grow"
             />
           </div>
           <div className="flex items-center">
             <label htmlFor="name" className="w-1/4">
-              <span className="text-blue-500">*</span>
+              <span className="text-accent">*</span>
               이름
             </label>
-            <input
-              type="text"
-              id="name"
-              className="border-b px-[40px] py-4 text-center grow"
-              placeholder="성함을 입력해 주세요."
-              ref={nameRef}
-            />
+            <Input variant="underline" id="name" placeholder="성함을 입력해 주세요." ref={nameRef} className="grow" />
           </div>
           <div className="flex items-center">
             <p className="w-1/4">성별</p>
@@ -155,13 +157,14 @@ const AccountForm = () => {
           <h3 className="py-5 border-b">부가정보</h3>
           <div className="flex items-center">
             <label htmlFor="tel" className="w-1/4">
-              <span className="text-blue-500">*</span>
+              <span className="text-accent">*</span>
               휴대폰번호
             </label>
-            <input
+            <Input
+              variant="underline"
               type="tel"
               id="tel"
-              className="border-b px-[40px] py-4 text-center grow"
+              className="grow"
               placeholder="전화번호를 입력해 주세요."
               ref={phoneRef}
             />
@@ -170,13 +173,14 @@ const AccountForm = () => {
 
         <div className="flex items-center">
           <label htmlFor="birth" className="w-1/4">
-            <span className="text-blue-500">*</span>
+            <span className="text-accent">*</span>
             생년월일
           </label>
-          <input
+          <Input
+            variant="underline"
             type="date"
-            id="birth"
-            className="border-b px-[40px] py-4 text-center grow"
+            id="tel"
+            className="grow justify-center"
             placeholder="생년월일을 입력해 주세요."
             ref={birthRef}
           />
