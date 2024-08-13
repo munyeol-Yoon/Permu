a72ef4fb89e7b80e08b2c86446dd53395f9643ab
muyeon <gmakin36@gmail.com>
2024-08-14 02:57:20 +0900

    Move: 결과페이지 layout 코드 이동

--- src/app/(provider)/(root)/category/search/result/layout.tsx
+++ src/app/(provider)/(root)/category/search/result/layout.tsx
@@ -1,30 +1,10 @@
 'use client';
 
-import { PropsWithChildren, useState } from 'react';
-import { FilterNavMenu, ResultFilter } from './_components';
+import { PropsWithChildren } from 'react';
 
 const ResultLayout = ({ children }: PropsWithChildren) => {
-  const [isFilterVisible, setIsFilterVisible] = useState(false);
-
-  const toggleFilterVisibility = () => {
-    setIsFilterVisible(!isFilterVisible);
-  };
-
   return (
     <>
-      <div className="relative flex items-center justify-between p-4 bg-gray-100">
-        <FilterNavMenu toggleFilterVisibility={toggleFilterVisibility}>가격</FilterNavMenu>
-
-        {isFilterVisible && (
-          <>
-            <div className="absolute top-full left-0 w-full max-w-[600px] bg-white rounded-lg shadow-lg z-50">
-              <ResultFilter />
-            </div>
-
-            <div className="absolute top-[calc(100%+20px)] left-0 w-full max-w-[600px] h-[4px] bg-white z-50"></div>
-          </>
-        )}
-      </div>
       <div>{children}</div>
     </>
   );
--- src/app/(provider)/(root)/category/search/result/page.tsx
+++ src/app/(provider)/(root)/category/search/result/page.tsx
@@ -5,10 +5,17 @@ import ProductCard from '@/components/Sliders/ProductCard';
 import { useSearchQuery } from '@/hooks/query';
 import { Product } from '@/types/products';
 import { useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import SearchNotFound from '../_components/SearchNotFound';
+import { FilterNavMenu, ResultFilter } from './_components';
 
 const ResultPageContent = () => {
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
+
+  const toggleFilterVisibility = () => {
+    setIsFilterVisible(!isFilterVisible);
+  };
+
   const searchParams = useSearchParams();
   const query = searchParams.get('query');
   const categoryId = searchParams.get('categoryId');
@@ -20,6 +27,19 @@ const ResultPageContent = () => {
 
   return (
     <>
+      <div className="relative flex items-center justify-between p-4 bg-gray-100">
+        <FilterNavMenu toggleFilterVisibility={toggleFilterVisibility}>가격</FilterNavMenu>
+
+        {isFilterVisible && (
+          <>
+            <div className="absolute top-full left-0 w-full max-w-[600px] bg-white rounded-lg shadow-lg z-50">
+              <ResultFilter />
+            </div>
+
+            <div className="absolute top-[calc(100%+20px)] left-0 w-full max-w-[600px] h-[4px] bg-white z-50"></div>
+          </>
+        )}
+      </div>
       <div className="container mx-auto p-5">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
           {data.data.map((productItem: Product) => (
